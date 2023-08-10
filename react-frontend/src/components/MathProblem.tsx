2772e8b4262ff1273dae7c8b8b9626b433f5d15c
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-10 13:04:09 +0200

    add timer to result submission
    The time a user has to submit an answer is now limited by a timer.

--- react-frontend/src/components/MathProblem.tsx
+++ react-frontend/src/components/MathProblem.tsx
@@ -1,23 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MathProblemGenerator from "../MathGeneration/MathProblemGenerator"
 
 function MathProblem() {
   const [userInput, setUserInput] = useState<number>(0)
-  const [score, setScore] = useState<number>(0)  
+  const [score, setScore] = useState<number>(0)   
   const [mathProblem, setMathProblem] = useState(MathProblemGenerator.generateProblem())
+  
+  // maximum amount of time (seconds) the user has to solve a math problem
+  const TIME_TO_SOLVE = 10
+  const [timeLeft, setTimeLeft] = useState<number>(TIME_TO_SOLVE)
 
   function handleSubmitResult(e: React.FormEvent) {
     e.preventDefault()
-
+    
+    // generate a new math problem, increase the users score and reset timer, if correct result is submitted
     if(userInput === mathProblem.result) {
       generateNewProblem()
-      // increase score, if correct result is submitted
+      setTimeLeft(TIME_TO_SOLVE)
       setScore(score + 1)
       return
     }
     // set score to 0, if the submitted result is incorrect
     setScore(0)
   }
+
+  // decrements the time the user has left for the current math problem by 1 second each second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(timeLeft > 1){
+        setTimeLeft(timeLeft => timeLeft - 1)
+      } else {
+        generateNewProblem()
+        setTimeLeft(TIME_TO_SOLVE)
+        setScore(0)
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  })
 
   function generateNewProblem() {
     const problem = MathProblemGenerator.generateProblem()
@@ -46,6 +65,9 @@ function MathProblem() {
       </form>
       <div>
         Current Score: {score}
+      </div>
+      <div>
+        Time left: {timeLeft}
       </div>
     </div>
   )
