e18891772efaf6a8ee0dcecfa671433eeb1566aa
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-14 13:11:48 +0200

    reorganize website into pages
    moved previous App.tsx content into a seperate HomePage component

---
+++ react-frontend/src/Pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import MathProblemWrapper from "../components/MathProblemWrapper"
+
+function HomePage() {
+  return(
+    <div className='h-screen grid grid-rows-3 bg-zinc-700'>
+      <div className='text-center text-7xl font-mono font-semibold mt-[5%] text-neutral-200 drop-shadow-[0_12px_25px_rgba(0,0,0,0.55)]'>
+        Mental Maths Trainer
+      </div>
+      <div className='flex items-center justify-center'>
+        <MathProblemWrapper/>
+      </div>
+      <div className='absolute bottom-0 w-full text-center'>© 2023 Zyransen</div>
+    </div>
+  )
+}
+
+export default HomePage
