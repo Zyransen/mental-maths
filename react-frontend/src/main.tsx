14d2ddab4a91cfef8478e1320a08d4f57fc2c835
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-14 15:37:39 +0200

    set up routing

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import HomePage from './Pages/HomePage'
+import GeneratorSettingsPage from './Pages/GeneratorSettingsPage'
+import NoPage from './Pages/NoPage'
+
 import './css/index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+    errorElement: <NoPage/>
+  },
+  {
+    path: "/settings",
+    element: <GeneratorSettingsPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
