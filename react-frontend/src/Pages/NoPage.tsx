0093e6b6e21cf68985faa60324177d20b5c1352c
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-14 13:13:18 +0200

    add NoPage component
    added a web page to be displayed when a page requested by a user does not exist

---
+++ react-frontend/src/Pages/NoPage.tsx
@@ -0,0 +1,13 @@
+function NoPage() {
+  return (
+    <div>
+      Sorry, the page you are trying to reach does not exist.
+
+      /*
+      Button: Take me back home
+      */
+    </div>
+  )
+}
+
+export default NoPage
