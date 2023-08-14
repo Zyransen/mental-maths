abc593f8ba7a552386ef63d80e2fad41e62fa5f4
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-14 15:55:08 +0200

    change background color

--- react-frontend/src/Pages/HomePage.tsx
+++ react-frontend/src/Pages/HomePage.tsx
@@ -2,7 +2,7 @@ import MathProblemWrapper from "../components/MathProblemWrapper"
 
 function HomePage() {
   return(
-    <div className='h-screen grid grid-rows-3 bg-zinc-700'>
+    <div className='h-screen grid grid-rows-3'>
       <div className='text-center text-7xl font-mono font-semibold mt-[5%] text-neutral-200 drop-shadow-[0_12px_25px_rgba(0,0,0,0.55)]'>
         Mental Maths Trainer
       </div>
