f2d2e8a870159202b25047bb821894fcb9453e10
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-15 22:36:28 +0200

    change size of title on HomePage for medium screens

--- react-frontend/src/Pages/HomePage.tsx
+++ react-frontend/src/Pages/HomePage.tsx
@@ -3,7 +3,7 @@ import MathProblemWrapper from "../components/MathProblemWrapper"
 function HomePage() {
   return(
     <div className='h-screen grid grid-rows-3'>
-      <div className='text-center text-5xl md:text-7xl font-mono font-semibold mt-[20%] md:mt-[5%] text-neutral-200 drop-shadow-[0_12px_25px_rgba(0,0,0,0.75)]'>
+      <div className='text-center text-5xl md:text-6xl lg:text-7xl font-mono font-semibold mt-[20%] md:mt-[12%] lg:mt-[5%] text-neutral-200 drop-shadow-[0_12px_25px_rgba(0,0,0,0.75)]'>
         Mental Maths Trainer
       </div>
       <div className='flex items-center justify-center'>
