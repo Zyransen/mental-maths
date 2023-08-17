1f0f2ea12bfe7b00a406e9e977432b85605a733c
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-17 12:11:28 +0200

    fix footer not sticking to bottom of viewport

--- react-frontend/src/components/Footer.tsx
+++ react-frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <div className='absolute bottom-0 w-full text-center'>
+    <div className='fixed bottom-0 flex items-center justify-center w-full h-[30px] bg-zinc-800 border-zinc-900 '>
       © 2023 Zyransen
     </div>
   )
