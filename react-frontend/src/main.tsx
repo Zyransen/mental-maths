84fd73df94cfdee256d97040238a413c0b81e8e3
Colin <122695176+Zyransen@users.noreply.github.com>
2023-08-17 12:40:01 +0200

    fix: does not show custom error page when page not found

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -18,7 +18,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/settings",
-    element: <SettingsPage/>
+    element: <SettingsPage/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "*",
+    element: <ErrorPage/>
   }
 ])
 
